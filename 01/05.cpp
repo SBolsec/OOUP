504ba613f64cd329c9742d611ab86c03f6881e4a
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-03-11 15:16:26 +0100

    peti zadatak

---
+++ 01/05.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class B
+{
+public:
+    virtual int prva() = 0;
+    virtual int druga(int) = 0;
+};
+
+class D : public B
+{
+public:
+    virtual int prva() { return 42; }
+    virtual int druga(int x) { return prva() + x; }
+};
+
+typedef int (*PFUN1)(B*);
+typedef int (*PFUN2)(B*, int);
+
+void function(B *pb) {
+    void** vptr = *(void***)pb;
+
+    PFUN1 f1 = (PFUN1)vptr[0];
+    PFUN2 f2 = (PFUN2)vptr[1];
+
+    std::cout << "Prva: " << f1(pb) << std::endl;
+    std::cout << "Druga: " << f2(pb, 10) << std::endl;
+}
+
+int main() {
+    B *pb = new D();
+    function(pb);
+    return 0;
+}
