304673bf3fe68b82d99fd5b403eced250458b860
Siniša Bolšec <sinisa.bolsec0@gmail.com>
2021-04-05 20:52:38 +0200

    5. zad gotov

--- 02/05.cpp
+++ 02/05.cpp
@@ -1,15 +1,76 @@
 #include <iostream>
+#include <list>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <fstream>
+#include <chrono>
+#include <ctime>
 
-// Classes for generating the numbers
+// For sleep function
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
 class IzvorBrojeva
 {
 public:
     virtual int generateNumber() = 0;
 };
 
+class Akcija
+{
+public:
+    virtual void update() = 0;
+};
+
+class SlijedBrojeva
+{
+private:
+    IzvorBrojeva *source;
+    std::list<Akcija *> actions;
+    std::vector<int> numbers;
+
+public:
+    SlijedBrojeva(IzvorBrojeva *source) : source{source} {}
+
+    std::vector<int> getNumbers() { return this->numbers; }
+
+    void attach(Akcija *action)
+    {
+        actions.push_back(action);
+    }
+    void dettach(Akcija *action)
+    {
+        actions.remove(action);
+    }
+    void notify()
+    {
+        std::list<Akcija *>::iterator it = actions.begin();
+        while (it != actions.end())
+        {
+            (*it)->update();
+            ++it;
+        }
+    }
+
+    void kreni()
+    {
+        while (true)
+        {
+            int number = source->generateNumber();
+            if (number == -1)
+                break;
+            numbers.push_back(number);
+            notify();
+            sleep(1);
+        }
+    }
+};
+
+// Implementations of number sources
 class TipkovnickiIzvor : public IzvorBrojeva
 {
 public:
@@ -76,25 +137,152 @@ public:
     }
 };
 
-class SlijedBrojeva
+// Implementations of actions
+class Zapisnik : public Akcija
 {
-protected:
-    std::vector<int> brojevi;
+private:
+    SlijedBrojeva *slijedBrojeva;
+    std::ofstream file;
 
 public:
+    Zapisnik(SlijedBrojeva *slijedBrojeva) : slijedBrojeva{slijedBrojeva}
+    {
+        this->slijedBrojeva->attach(this);
+        try
+        {
+            file.open("log.txt");
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            throw e;
+        }
+    }
+    ~Zapisnik()
+    {
+        this->slijedBrojeva->dettach(this);
+        if (file.is_open())
+            file.close();
+    }
+
+    virtual void update()
+    {
+        if (!file.is_open()) return;
+
+        std::vector<int> numbers = slijedBrojeva->getNumbers();
+        std::vector<int>::iterator it = numbers.begin();
+        while (it != numbers.end())
+        {
+            file << *it.base() << " ";
+            ++it;
+        }
+
+        std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        file << std::ctime(&time);
+        file.flush();
+    }
 };
 
+class IspisiSumu : public Akcija
+{
+private:
+    SlijedBrojeva *slijedBrojeva;
+
+public:
+    IspisiSumu(SlijedBrojeva *slijedBrojeva) : slijedBrojeva{slijedBrojeva}
+    {
+        this->slijedBrojeva->attach(this);
+    }
+    ~IspisiSumu()
+    {
+        this->slijedBrojeva->dettach(this);
+    }
+
+    virtual void update()
+    {
+        std::vector<int> numbers = slijedBrojeva->getNumbers();
+        std::vector<int>::iterator it = numbers.begin();
+        int sum = 0;
+        while (it != numbers.end())
+        {
+            sum += *it.base();
+            ++it;
+        }
+        std::cout << "Suma brojeva: " << sum << std::endl;
+    }
+};
+
+class IspisiProsjek : public Akcija
+{
+private:
+    SlijedBrojeva *slijedBrojeva;
+
+public:
+    IspisiProsjek(SlijedBrojeva *slijedBrojeva) : slijedBrojeva{slijedBrojeva}
+    {
+        this->slijedBrojeva->attach(this);
+    }
+    ~IspisiProsjek()
+    {
+        this->slijedBrojeva->dettach(this);
+    }
+
+    virtual void update()
+    {
+        std::vector<int> numbers = slijedBrojeva->getNumbers();
+        std::vector<int>::iterator it = numbers.begin();
+        int sum = 0;
+        while (it != numbers.end())
+        {
+            sum += *it.base();
+            ++it;
+        }
+        std::cout << "Prosjek brojeva: " << (double)sum / numbers.size() << std::endl;
+    }
+};
+
+class IspisiMedijan : public Akcija
+{
+private:
+    SlijedBrojeva *slijedBrojeva;
+
+public:
+    IspisiMedijan(SlijedBrojeva *slijedBrojeva) : slijedBrojeva{slijedBrojeva}
+    {
+        this->slijedBrojeva->attach(this);
+    }
+    ~IspisiMedijan()
+    {
+        this->slijedBrojeva->dettach(this);
+    }
+
+    virtual void update()
+    {
+        std::vector<int> numbers = slijedBrojeva->getNumbers();
+        sort(numbers.begin(), numbers.end());
+        int n = numbers.size();
+        double median;
+
+        if (n == 1)
+            median = numbers.front();
+        else if (n % 2 != 0)
+            median = numbers.at(n / 2);
+        else
+            median = (numbers.at(n / 2) + numbers.at(n / 2 - 1)) / 2.0;
+
+        std::cout << "Medijan brojeva: " << median << std::endl;
+    }
+};
+
+// Starting point
 int main()
 {
-    IzvorBrojeva *izvor = new DatotecniIzvor("/data/projekti/OOUP/02/05-brojevi.txt");
-    for (int i = 0; i < 9; i++)
-    {
-        std::cout << izvor->generateNumber() << std::endl;
-    }
-    delete izvor;
-    IzvorBrojeva *izvor1 = new TipkovnickiIzvor();
-    std::cout << izvor1->generateNumber() << std::endl;
-    std::cout << izvor1->generateNumber() << std::endl;
-    std::cout << izvor1->generateNumber() << std::endl;
+    SlijedBrojeva *slijedBrojeva = new SlijedBrojeva(new DatotecniIzvor("./02/05-brojevi.txt"));
+    Akcija *a1 = new IspisiSumu(slijedBrojeva);
+    Akcija *a2 = new IspisiProsjek(slijedBrojeva);
+    Akcija *a3 = new IspisiMedijan(slijedBrojeva);
+    Akcija *a4 = new Zapisnik(slijedBrojeva);
+    slijedBrojeva->kreni();
+
     return 0;
 }
